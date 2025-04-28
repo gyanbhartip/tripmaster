93cc8516a46d64c79c182933ef5b2d6956badd37
Gyan Bharti <gyanbhartip@gmail.com>
2025-04-28 11:07:23 +0530

    homepage helpers

---
+++ app/routes/root/page-layout.tsx
@@ -0,0 +1,33 @@
+import { logoutUser } from '@/appwrite/auth';
+import { useNavigate } from 'react-router';
+
+const PageLayout = () => {
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        await logoutUser();
+        navigate('/sign-in');
+    };
+    return (
+        <div>
+            <button
+                onClick={handleLogout}
+                className="cursor-pointer"
+                type="button">
+                <img
+                    src="/assets/icons/logout.svg"
+                    alt="logout"
+                    className="size-6"
+                />
+            </button>
+            <button
+                type="button"
+                onClick={() => {
+                    navigate('/dashboard');
+                }}>
+                Dashboard
+            </button>
+        </div>
+    );
+};
+
+export default PageLayout;
